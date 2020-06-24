54dbb1f39dd1472063f2d066a9fd15272d75a787
tatianalopes <tcalopes19@gmail.com>
2020-06-24 19:18:04 -0300

    Configure code analysis tools - EditorConfig, ESLint and Prettier

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -3,36 +3,36 @@ import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
 interface CreateAppoiuntmentDTO {
-    provider: string;
-    date: Date;
+  provider: string;
+  date: Date;
 }
 
 class AppointmentsRepository {
-    private appointments: Appointment[];
+  private appointments: Appointment[];
 
-    constructor() {
-        this.appointments = [];
-    }
+  constructor() {
+    this.appointments = [];
+  }
 
-    public getAll(): Appointment[] {
-        return this.appointments;
-    }
+  public getAll(): Appointment[] {
+    return this.appointments;
+  }
 
-    public findByDate(date: Date): Appointment | null {
-        const findAppointment = this.appointments.find(appointment =>
-            isEqual(date, appointment.date),
-        );
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
 
-        return findAppointment || null;
-    }
+    return findAppointment || null;
+  }
 
-    public create({ provider, date }: CreateAppoiuntmentDTO): Appointment {
-        const appointment = new Appointment({ provider, date });
+  public create({ provider, date }: CreateAppoiuntmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
-        this.appointments.push(appointment);
+    this.appointments.push(appointment);
 
-        return appointment;
-    }
+    return appointment;
+  }
 }
 
 export default AppointmentsRepository;
