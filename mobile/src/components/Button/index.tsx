06b2e6be39a21c648e996124fb3778abbe9af497
tatianalopes <tcalopes19@gmail.com>
2020-07-31 23:03:48 -0300

    Add mobile project

---
+++ mobile/src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+}
+
+const Input: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Container {...rest}>
+      <ButtonText>
+        {children}
+      </ButtonText>
+    </Container>
+  );
+};
+
+export default Input;
