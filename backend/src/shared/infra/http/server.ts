22af55bc1394dcfab2a6aac075e88391f4a7aa4b
tatianalopes <tcalopes19@gmail.com>
2020-08-07 22:31:04 -0300

    Refactor backend to follow DDD architecture

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -5,10 +5,11 @@ import 'express-async-errors';
 import cors from 'cors';
 
 import routes from './routes';
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
 import handleErrors from './middlewares/handleErrors';
 
-import './database';
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
