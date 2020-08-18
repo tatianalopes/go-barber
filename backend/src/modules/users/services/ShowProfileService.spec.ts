3e4262c5a707dee0a3e85466ccc9fe4a27a0c127
tatianalopes <tcalopes19@gmail.com>
2020-08-17 22:56:29 -0300

    Implement profile update functionality

---
+++ backend/src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+
+import ShowProfileService from "./ShowProfileService";
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AppError from "@shared/errors/AppError";
+
+let fakeUsersRepository: FakeUsersRepository;
+
+let showProfile: ShowProfileService;
+
+describe('ShowProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able to show profile', async () => {
+    const name = 'John Doe';
+    const email = 'johndoe@example.com';
+    const password = '123456';
+    const user = await fakeUsersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    const profile = await showProfile.execute(user.id);
+
+    expect(profile.name).toBe(name);
+    expect(profile.email).toBe(email);
+  });
+
+  it('should not be able to show profile from a non-existing user', async () => {
+    await expect(
+      showProfile.execute('non-existing user ID')
+    ).rejects.toBeInstanceOf(AppError);
+  });
+})
---
+++ backend/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,56 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, name, email, old_password, password }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('User not found.', 401);
+    }
+
+    const userWithUpdateEmail = await this.usersRepository.findByEmail(email);
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user_id) {
+      throw new AppError('Email already in use.');
+    }
+
+    user.name = name;
+    user.email= email;
+    if (password) {
+      if (!old_password) {
+        throw new AppError('You need to inform the old password to set a new password.');
+      }
+
+      const checkOldPassword = await this.hashProvider.compareHash(old_password, user.password);
+      if (!checkOldPassword) {
+        throw new AppError('Old password does not match.');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    return this.usersRepository.update(user);
+  }
+}
+
+export default UpdateProfileService;
