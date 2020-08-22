652c2ed45a699a1e8fe0490649bb91a37b6663e3
tatianalopes <tcalopes19@gmail.com>
2020-08-22 16:44:24 -0300

    Implement functionality to list appointments of a provider

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata";
+
+import ListProviderAppointmentsService from "./ListProviderAppointmentsService";
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(fakeAppointmentsRepository);
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const provider_id = 'provider_id';
+    const user_id = 'user_id';
+    const day = 20;
+    const month = 4;
+    const year = 2020;
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: new Date(year, month-1, day, 14, 0, 0),
+    });
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: new Date(year, month-1, day, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    expect(appointments).toEqual(
+      expect.arrayContaining([
+        appointment1,
+        appointment2,
+      ]),
+    );
+  });
+})
