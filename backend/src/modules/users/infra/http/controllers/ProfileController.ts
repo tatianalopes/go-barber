4346c8bdb4ef2dae39b2a227166e3ef824312507
tatianalopes <tcalopes19@gmail.com>
2020-08-24 21:05:30 -0300

    Use Class Tranformer

--- backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -27,8 +28,7 @@ export default class ProfileController {
       old_password,
       password,
     });
-    delete user.password;
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
